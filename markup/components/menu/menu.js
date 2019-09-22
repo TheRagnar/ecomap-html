c93dc5222a53c8dbab3005cd26d841b8f45eaa2f
@georgekonoshenko <pebah@inbox.ru>
2019-09-23 01:57:02 +0600

    #add_header

---
+++ markup/components/menu/menu.js
@@ -0,0 +1,35 @@
+
+import $ from 'jquery'
+
+$(document).ready(function(){
+    calcWidth();
+    $('.menu__dots').click(function(){
+        $(this).closest('.menu').toggleClass('active');
+    })
+    $(document).mouseup(function (e){
+		var div = $(".menu");
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) {
+			$('.menu').removeClass('active');
+		}
+	});
+})
+// $(window).resize(function(){
+//     calcWidth();
+// })
+
+function calcWidth(){
+    var maxWidth = $('.header__container .menu__items').outerWidth() + 30;
+    var wdh = 0;
+    $('.header__container .menu__item').each(function(){
+        wdh+=$(this).outerWidth();
+        if(wdh > maxWidth) {
+            $(this).appendTo($('.header__container .menu__more'))
+        }
+    })
+    if(wdh > maxWidth){
+        $('.menu__dots').show();
+    } else {
+        $('.menu__dots').hide();
+    }
+}
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
-
+import 'jquery'
+import '../../components/menu/menu';
+import '../../components/header/header';
 /*
     This file can be used as entry point for webpack!
  */
