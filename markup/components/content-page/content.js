cc80403ad8719b21a840c6f33da349bfc2f797c8
mashakorotia <mashakorotia@gmail.com>
2019-10-28 02:07:58 +0200

    #102836/newwork ЭП: Верстка: Сверстать макеты для личного кабинета пользователя (ЭП)

--- markup/components/content-page/content.js
+++ markup/components/content-page/content.js
@@ -1,7 +1,12 @@
 import $ from 'jquery'
 
-$('.form-group__input').on('change',function(){
-    if( $(this).val() != '' ) {
+
+if ( !($('.form-group .form-group__input').val() == '') ) {
+    $(this).parent().addClass('not-empty');
+};
+
+$('.form-group__input, .form-group__textarea').on('change', function () {
+    if ($(this).val() != '') {
         $(this).parent().addClass('not-empty');
     } else {
         $(this).parent().removeClass('not-empty');
@@ -11,3 +16,4 @@ $('.form-group__input').on('change',function(){
 $('.select').select2({
     minimumResultsForSearch: -1,
 });
+
